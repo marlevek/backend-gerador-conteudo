7232053ab0bb8082187983643c0f3f18be7b8b02
marlevek <marcelo_levek@hotmail.com>
2025-12-16 15:43:37 -0300

    add whitenoise for static files

--- core/settings.py
+++ core/settings.py
@@ -58,6 +58,7 @@ SIMPLE_JWT = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -134,6 +135,10 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+CORS_ALLOWED_ORIGINS = [
+    "https://seusite.com",  # Substitua pelo seu domínio real
+    "https://www.seusite.com",
+]
 
-CORS_ALLOW_ALL_ORIGINS = True
